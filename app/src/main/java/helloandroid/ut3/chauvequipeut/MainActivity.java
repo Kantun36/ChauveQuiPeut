d22c63b102d340e4836f621316120067a5800c4f
Quentin Jouannot <quentin.jouannot@gmail.com>
2024-03-22 09:25:26 +0100

    Menu

--- app/src/main/java/helloandroid/ut3/chauvequipeut/MainActivity.java
+++ app/src/main/java/helloandroid/ut3/chauvequipeut/MainActivity.java
@@ -1,18 +1,49 @@
 package helloandroid.ut3.chauvequipeut;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new GameView(this));
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+        Button optionButton = findViewById(R.id.optionButton);
+        Button quitButton = findViewById(R.id.quitButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Start button clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, GameView.class);
+                startActivity(intent);
+            }
+        });
+
+        optionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Option button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
+
